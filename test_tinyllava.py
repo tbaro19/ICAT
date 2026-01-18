f0d3f608b9ab12160412bf47196a21a125102588
tbaro19 <tbaro19@users.noreply.github.com>
2026-01-18 22:15:43 +0700

    chore: Remove test file

--- test_tinyllava.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Test TinyLLaVA model loading"""
-
-from src.models.tinyllava_wrapper import TinyLLaVAWrapper
-
-print("Loading TinyLLaVA...")
-wrapper = TinyLLaVAWrapper()
-print("SUCCESS: TinyLLaVA loaded!")
