22a86b9e2ff0b8dca08b7c102cf8807197bc922b
tbaro19 <tbaro19@users.noreply.github.com>
2026-01-18 21:58:39 +0700

    fix: Update __init__.py to import TinyLLaVA instead of InternVL2

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,7 +2,7 @@
 Vision-Language Model Wrappers for Black-box Attacks
 """
 from .vlm_base import VLMBase
-from .internvl2_wrapper import InternVL2Wrapper
+from .tinyllava_wrapper import TinyLLaVAWrapper
 from .qwen2vl_wrapper import Qwen2VLWrapper
 
-__all__ = ['InternVL2Wrapper', 'Qwen2VLWrapper']
+__all__ = ['TinyLLaVAWrapper', 'Qwen2VLWrapper']
